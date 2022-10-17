746cf83b869cd92974260fe88a33575923d558d6
Lucas Vazquez <vazquez.lucas.alejo@gmail.com>
2022-10-17 02:55:22 -0300

    TP 11 casi :c

---
+++ TP11/script.js
@@ -0,0 +1,24 @@
+function calc(){
+    let pOperando;
+    let sOperando
+
+
+    const suma 
+    const resta
+    const multiplicacion
+    const division
+
+    result = pOperando + sOperando
+    result = pOperando - sOperando
+    if(sOperando==0)
+    "Error, no se puede dividir por 0. Ingrese un valor correcto"
+    result = pOperando / sOperando
+    result = pOperando * sOperando
+    result = 10
+    let result = document.getElementById("resultado")
+    result.textContent = 1
+}
+calc();
+
+
+
